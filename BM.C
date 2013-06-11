24bbcdde96fe142f03e6fac90672dd115b280cbc
Karimkhan <karimkhan_it@yahoo.com>
2013-06-11 10:39:09 +0530

    Boyen Moore completed

--- BM.C
+++ BM.C
@@ -1,5 +1,3 @@
-//Partially completed.
-//Works for statically given string. Need to be improved for user given string and all kinds of text strings
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,11 +9,11 @@ char *BoyerMoore( unsigned char *data, unsigned int dataLength, unsigned char *s
     register unsigned char lastChar;
 
     if (strLength == 0)
-        return NULL;
+	return NULL;
 
     // Initialize skip lookup table
     for (i = 0; i < 256; i++)
-        skipTable[i] = strLength;
+	skipTable[i] = strLength;
 
     search = string;
 
@@ -24,7 +22,7 @@ char *BoyerMoore( unsigned char *data, unsigned int dataLength, unsigned char *s
 
     do
     {
-        skipTable[*search++] = i;
+	skipTable[*search++] = i;
     } while (i--);
 
     lastChar = *--search;
@@ -35,62 +33,67 @@ char *BoyerMoore( unsigned char *data, unsigned int dataLength, unsigned char *s
 
     while ((int)dataLength > 0 )
     {
-        unsigned int skip;
+	unsigned int skip;
 
-        skip = skipTable[*search];
-        search += skip;
-        dataLength -= skip;
-        skip = skipTable[*search];
-        search += skip;
-        dataLength -= skip;
-        skip = skipTable[*search];
+	skip = skipTable[*search];
+	search += skip;
+	dataLength -= skip;
+	skip = skipTable[*search];
+	search += skip;
+	dataLength -= skip;
+	skip = skipTable[*search];
 
-        if (*search != lastChar) /*if (skip > 0)*/
-        {
-            // Character does not match, realign string and try again
-            search += skip;
-            dataLength -= skip;
-            continue;
-        }
+	if (*search != lastChar) /*if (skip > 0)*/
+	{
+	    // Character does not match, realign string and try again
+	    search += skip;
+	    dataLength -= skip;
+	    continue;
+	}
 
-        // We had a match, we could be at the end of the string
-        i = strLength;
+	// We had a match, we could be at the end of the string
+	i = strLength;
 
-        do
-        {
-            // Have we found the entire string?
-            if (i-- == 0)
-                return search;
-        } while (*--search == string[i]);
+	do
+	{
+	    // Have we found the entire string?
+	    if (i-- == 0)
+		return search;
+	} while (*--search == string[i]);
 
-        // Skip past the part of the string that we scanned already
-        search += (strLength - i + 1);
-        dataLength--;
+	// Skip past the part of the string that we scanned already
+	search += (strLength - i + 1);
+	dataLength--;
     }
 
     // We reached the end of the data, and didn't find the string
     return NULL;
 }
-
+void chomp(char *s) {
+    int n = strlen(s);
+    while (n && (s[n-1]==10 || s[n-1]==13)) s[--n] = 0;
+}
 int main(void)
 {
     char target[200];
-	char *ch = target;
+	char *ch = target,*str;
 	char pattern[20];
 	int i,k,count,l;
-	char *str = BoyerMoore( target, strlen(target), pattern, strlen(pattern) );
+	chomp(target);
+	chomp(pattern);
+	str = BoyerMoore( target, strlen(target), pattern, strlen(pattern) );
 	printf("Enter the string: \n");
-	//fgets(target,100,stdin);
-	scanf ("%[^\n]%*c", target);
+	fgets(target,100,stdin);
+	//scanf ("%[^\n]%*c", target);
 	printf("Enter the string to be matched: \n");
-//	fgets(pattern,20,stdin);
-	scanf ("%[^\n]%*c", pattern);
+	fgets(pattern,20,stdin);
+	//scanf ("%[^\n]%*c", pattern);
 
 
     if (str == NULL)
-        puts( "String not found" );
+	puts( "String not found" );
     else
-        puts( str );
+	puts( "true" );
     getch();
     return 0;
 }
